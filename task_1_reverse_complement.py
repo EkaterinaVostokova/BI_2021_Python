6c55a194139d22aa800470acda064bbd13af9e2c
EkaterinaVostokova <90833922+EkaterinaVostokova@users.noreply.github.com>
2021-10-04 13:08:46 +0300

    Update task_1_reverse_complement.py

--- task_1_reverse_complement.py
+++ task_1_reverse_complement.py
@@ -8,27 +8,27 @@
 def transcribe (x):
     result = x.replace ("T", "U")
     result1 = result.replace ("t", "u")
-    return (result1)
+    return result1
 def reverse (x):
     result1 = x[::-1]
-    return (result1)
+    return result1
 def complement (x):
-    result = ""
+    result = []
     #in this case if neither t or u is present, it is considered as a DNA sequence
     if ("t" in x or "T" in x) and ("u" not in x and "U" not in x) or ("t" not in x and "T" not in x) and ("u" not in x and "U" not in x):
         #here the first element is complementary to the last one, etc.
         al = ["A", "a", "C", "c", "g", "G", "t", "T"]
         for element in x:
-            result += al[len(al) - 1 - al.index (element)]
-        return (result)
+            result.append(al[len(al) - 1 - al.index (element)])
+        return ("".join(result))
     elif ("u" in x or "U" in x) and ("t" not in x and "T" not in x)==True:
         al = ["A", "a", "C", "c", "g", "G", "u", "U"]
         for element in x:
-            result += al[len(al) - 1 - al.index (element)]
-        return (result)
+            result.append(al[len(al) - 1 - al.index (element)])
+        return ("".join(result))
 def reverse_complement (x):
     result = complement (x)
-    return (result[::-1])
+    return result[::-1]
 #the program itself starts
 command = ""
 while command != "exit":
