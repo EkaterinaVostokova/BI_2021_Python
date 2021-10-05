e5f53d557334f7d6b25f04b9356f38f96b4b142c
EkaterinaVostokova <90833922+EkaterinaVostokova@users.noreply.github.com>
2021-10-05 09:00:55 +0300

    Update and rename units_converter_1.py to units_converter.py

---
+++ units_converter.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+# goal: find out most used American values and convert them into European ones
+
+
+data = input("What to convert? American -> European. Possible: temperature, distance, velocity")
+if data == "temperature":
+    temperature = (float(input ("What is the temperature in Fahrenheit? ")) - 32) * (5/9)
+    print(temperature, "celsius")
+elif data == "distance":
+    distance_type = input("League, Mile, Foot, Caliber or Inch? ")
+    distance = float(input("How many?"))
+    if distance_type == "League":
+        distance = distance * 4828.032
+        print(distance, "meters")
+    elif distance_type == "Mile":
+        distance = distance * 1609.344
+        print(distance, "meters")
+    elif distance_type == "Foot":
+        distance = distance * 0.3048
+        print(distance, "meters")
+    elif distance_type == "Inch":
+        distance = distance * 0.0254
+        print(distance, "meters")
+    elif distance_type == "Caliber":
+        distance = distance * 0.000254
+        print(distance, "meters")
+    else:
+        print("Unknown distance")
+elif data == "velocity":
+    velocity = float(input("What is the velocity in miles per second? ")) * 1609.34401
+    print(velocity, "meters per second")
