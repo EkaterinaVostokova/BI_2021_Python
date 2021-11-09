32d7aefea12107e2eeba5b4dfea196397c6c0542
EkaterinaVostokova <90833922+EkaterinaVostokova@users.noreply.github.com>
2021-11-09 19:18:49 +0300

    Update numpy_challenge.py

--- numpy_challenge.py
+++ numpy_challenge.py
@@ -3,17 +3,19 @@
 
 # In[3]:
 
+import numpy as np
 
-def matrix_multiplication (a, b):
-    return np.dot (a, b)
+
+def matrix_multiplication(a, b):
+    return np.dot(a, b)
 
 
 # In[4]:
 
 
-def multiplication_check (list_matrices):
+def multiplication_check(list_matrices):
     c = 0
-    for i in range (len(list_matrices)-1):
+    for i in range(len(list_matrices)-1):
         # число столбцов первого равно числу строк второго
         if len(list_matrices[i][0]) == len(list_matrices[i+1]):
             c+=1
@@ -26,24 +28,24 @@ def multiplication_check (list_matrices):
 # In[5]:
 
 
-def multiply_matrices (list_matrices):
-    mult = np.linalg.multi_dot (list_matrices)
+def multiply_matrices(list_matrices):
+    mult = np.linalg.multi_dot(list_matrices)
     return mult
 
 
 # In[ ]:
 
 
-def compute_2d_distance (a, b):
-    return np.sqrt( ((a[0]-b[0])**2)+((a[1]-b[1])**2) )
+def compute_2d_distance(a, b):
+    return np.sqrt(((a[0]-b[0])**2)+((a[1]-b[1])**2))
 
 
 # In[ ]:
 
 
-def compute_multidimensional_distance (a, b):
+def compute_multidimensional_distance(a, b):
     d = 0
-    for i in range (len(a)):
+    for i in range(len(a)):
         d += (a[i]-b[i])**2
     return np.sqrt(d)
 
@@ -54,7 +56,6 @@ def compute_multidimensional_distance (a, b):
 # compute pair distances не получилась, не очень поняла, что именно будет парами, для которых нужны попарные расстояния
 
 if __name__ == "__main__":
-    n1 = np.arange (50, 2, -3)
+    n1 = np.arange(50, 2, -3)
     n2 = np.array([[0, 1, 2], [3, 4, 5]])
     n3 = np.linspace(0, 1, 6)
-
