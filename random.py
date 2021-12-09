263ae418ebfa92449d0ff74bfd43c862c1cc8239
EkaterinaVostokova <90833922+EkaterinaVostokova@users.noreply.github.com>
2021-12-09 19:40:24 +0300

    Update random.py

--- random.py
+++ random.py
@@ -35,8 +35,8 @@ for i in range(10, 10**7, 1000000):
     np.random.uniform(0, 1, (1, i))
     times_numpy.append(time.time() - start_time)
 figure(figsize=(15, 12), dpi=80)
-plt.plot(i_s, times_random, label = "random")
-plt.plot(i_s, times_numpy, label = "numpy")
+plt.plot(i_s, times_random, label="random")
+plt.plot(i_s, times_numpy, label="numpy")
 plt.legend()
 plt.xlabel('times of iteration')
 plt.ylabel('time of function')
@@ -56,23 +56,25 @@ def check_sorted(a):
         if (a[i] > a[i+1]):
             return False
     return True
+
+
 def monkey_sort(a):
     while not check_sorted(a):
         random.shuffle(a)
 
-        
+
 mycode = '''
 def monkey_sort(a):
     while not check_sorted(a):
         random.shuffle(a)
 '''
- 
+
 # list 'a' is necessary
 
 max_value = 60
 out = []
 out_std = []
-for i in range (10, max_value, 10):
+for i in range(10, max_value, 10):
     b = timeit.repeat(stmt=mycode, repeat=i)
     out.append(np.mean(b))
     out_std.append(np.std(b))
@@ -118,15 +120,15 @@ plt.scatter(x, y)
 
 
 n = 1000
-a = np.zeros ((n, n))
-points = np.array ([(0, 0), (0, n), (n, n//2)])
+a = np.zeros((n, n))
+points = np.array([(0, 0), (0, n), (n, n//2)])
 start = np.array((0, 0))
 for i in range(n*n):
     new_point = random.randint(0, 2)
     start = (points[new_point]+start)//2
     a[start[0]][start[1]] = 1
 figure(figsize=(12, 10), dpi=80)
-a = np.flip(a, axis = 0)
+a = np.flip(a, axis=0)
 plt.imshow(a)
 
 
@@ -145,15 +147,14 @@ ls1 = []
 for word in ls:
     if len(word) > 2:
         if word[-1] in string.punctuation:
-        # if the last element is dot or comma
-            word1 = ''.join(random.sample(word[1:-2],len(word)-3))
-            word = word.replace (word[1:-2], word1)
+            # if the last element is dot or comma
+            word1 = ''.join(random.sample(word[1:-2], len(word)-3))
+            word = word.replace(word[1:-2], word1)
             ls1.append(word)
         else:
-            word1 = ''.join(random.sample(word[1:-1],len(word)-2))
-            word = word.replace (word[1:-1], word1)
+            word1 = ''.join(random.sample(word[1:-1], len(word)-2))
+            word = word.replace(word[1:-1], word1)
             ls1.append(word)
     else:
         ls1.append(word)
 print(*ls1)
-
